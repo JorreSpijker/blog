caee41cd64ee339fb4816c83303c1615f1a46aba
Jorre Spijker <jorre@linku.nl>
2022-07-05 10:20:04 +0200

    removed all

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import sitemap from '@astrojs/sitemap';
-import svelte from '@astrojs/svelte';
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://example.com',
-  integrations: [sitemap(), svelte()],
-});
